2a0a549e0d923b4a9b6e70d43a593705550d1453
Gyanendra prakash <pgyanendra333@gmail.com>
2022-12-21 04:18:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import ToDoForm from "./components/ToDoForm/ToDoForm";
+import ToDoList from "./components/ToDoList/ToDoList";
+
+function App() {
+  return (
+    <div className="container">
+      <ToDoForm />
+      <ToDoList />
+    </div>
+  );
+}
+
+export default App;
